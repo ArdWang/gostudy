c432f9c95f8ffec93b95d594f4db519502e5b2da
Jing Wang <278161009@qq.com>
2020-08-12 19:07:21 +0800

    update rnd

--- maze/maze.go
+++ maze/maze.go
@@ -19,7 +19,7 @@ func readMaze(filename string) [][] int {
 	for i := range maze {
 		maze[i] = make([]int, col)
 		for j := range maze[i] {
-			fmt.Fscanf(file, "%d", maze[i][j])
+			fmt.Fscanf(file, "%d", &maze[i][j])
 		}
 	}
 
@@ -40,39 +40,71 @@ func (p ponit) add(r ponit) ponit{
 }
 
 func (p ponit) at(grid [][] int) (int,bool) {
-	return grid[p.i][p.j]
+	if p.i < 0 || p.i >= len(grid) {
+		return 0, false
+	}
+
+	if p.j < 0 || p.j >= len(grid[p.i]){
+		return 0,false
+	}
+
+	return grid[p.i][p.j], true
 }
 
-func walk(maze [][] int, start, end ponit) {
+func walk(maze [][] int, start, end ponit) [][] int{
 	steps := make([][] int, len(maze))
 	for i := range steps {
 		steps[i] = make([]int, len(maze[i]))
 	}
+
 	Q := [] ponit{start}
 
 	for len(Q) > 0 {
 		cur := Q[0]
 		Q = Q[1:]
 
+		if cur == end {
+			break
+		}
+
 		for _, dir := range dirs {
 			next := cur.add(dir)
 
-			// maze at next is 0
-			// and steps at next is 0
-			// and next != start
+			val, ok := next.at(maze)
+
+			if !ok || val == 1 {
+				continue
+			}
+
+			val, ok = next.at(steps)
+
+			if !ok || val != 0 {
+				continue
+			}
+
+			if next == start {
+				continue
+			}
+
+			curSteps, _ := cur.at(steps)
+			steps[next.i][next.j] = curSteps + 1
+
+			Q = append(Q, next)
+
 		}
 	}
+	return steps
 }
 
 
 func main() {
 	 maze := readMaze("maze/maze.in")
-	 for _, row := range maze{
+	 steps := walk(maze, ponit{0,0}, ponit{len(maze)-1, len(maze[0])-1})
+
+	 for _, row := range steps {
 	 	for _, val := range row {
-	 		fmt.Printf("%d", val)
+	 		fmt.Printf("%3d",val)
 		}
 		fmt.Println()
 	 }
-
-	 walk(maze, ponit{0,0}, ponit{len(maze)-1, len(maze[0])-1})
 }
